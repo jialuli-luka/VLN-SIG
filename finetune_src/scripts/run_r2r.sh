603bbd2f40fe47b62c787af567d0cef5a462b301
jialuli-luka <790359064@qq.com>
2023-08-19 00:31:15 -0400

    update config

--- finetune_src/scripts/run_r2r.sh
+++ finetune_src/scripts/run_r2r.sh
@@ -57,4 +57,4 @@ CUDA_VISIBLE_DEVICES='0' python r2r/main.py $flag  \
       --ig_head 8192 \
       --ig_path ../dvae_probs.hdf5 \
       --weighted_token \
-      --train_ig 0.7
+      --train_ig 0.6
